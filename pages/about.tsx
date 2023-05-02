51b5cfdae7e1729584d51a388f39b555f8dd0d92
kkbiker <nakayosikouyama.kenta@gmail.com>
2023-05-03 00:50:15 +0900

    🚀 練習

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>index page</title>
+        <title>about page</title>
       </Head>
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
